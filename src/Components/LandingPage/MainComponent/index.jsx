5ae1af3047b1d07336e4bb3b715340f3cb7bef6e
D32SSV <131158288+D32SSV@users.noreply.github.com>
2024-01-11 18:51:23 +0530

    padding

--- src/Components/LandingPage/MainComponent/index.jsx
+++ src/Components/LandingPage/MainComponent/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./styles.css";
 import Button from "../../Common/Button";
-import grad from "../../../assets/gradient.png"
-import iphone from "../../../assets/iphone.png"
+import grad from "../../../assets/gradient.png";
+import iphone from "../../../assets/iphone.png";
 
 function MainComponent() {
   return (
@@ -12,13 +12,13 @@ function MainComponent() {
         <h1 className="realtime">Real Time.</h1>
         <p className="text">Visit the Dashboard & track crypto in real time</p>
         <div className="btnDiv">
-            <Button text={"Dashboard"}/>
-            <Button text={"share"} outLined={true}/>
+          <Button text={"Dashboard"} />
+          <Button text={"share"} outLined={true} />
         </div>
       </div>
       <div className="rightdiv">
-        <img src={iphone} className="iphone"/>
-        <img src={grad} className="grad"/>
+        <img src={iphone} className="iphone" />
+        <img src={grad} className="grad" />
       </div>
     </div>
   );
